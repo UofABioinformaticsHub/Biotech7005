e566abbafbd2a0196a16c045d064d905f095e844
steveped <stephen.pederson.au@gmail.com>
2018-09-04 11:50:47 +0930

    Added A4. Download links are not yet live

---
+++ Assignments/A4_id2Gene.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(pander)
+
+# Run this from the project root directory
+
+# First download the gff:
+system2("wget",
+"ftp://ftp.ensemblgenomes.org/pub/plants/current/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.40.gff3.gz
+")
+
+# Extract the gene IDs
+system2("zcat",  "Arabidopsis_thaliana.TAIR10.40.gff3.gz | egrep '.+\s.+\sgene\s' | cut -f9 | sed -r 's/ID=gene:([^;]+);.+/\1/g' > Assignments/AthalianaIDs.txt
+")
+
+# Now assign each student ID to a gene:
+ids <- read_tsv("enrolments.tsv")
+genes <- readLines("Assignments/AthalianaIDs.txt")
+set.seed(7005)
+tibble(ID = paste0("a", ids$Emplid),
+			 Gene = sample(genes, size = nrow(ids))) %>%
+	pander(style = "rmarkdown",
+				 justify = "ll")
