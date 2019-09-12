a336205582c7d7341dd9f81b8417a934a8715a17
Dan Kortschak <dan@kortschak.io>
2019-09-12 15:47:14 +0930

    assignments: fix bRocken R code, but I Repeat myself

--- Assignments/A4_id2Gene.R
+++ Assignments/A4_id2Gene.R
@@ -4,16 +4,13 @@ library(pander)
 # Run this from the project root directory
 
 # First download the gff:
-system2("wget",
-"ftp://ftp.ensemblgenomes.org/pub/plants/current/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.40.gff3.gz
-")
+system2("wget", "ftp://ftp.ensemblgenomes.org/pub/plants/current/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.44.gff3.gz")
 
 # Extract the gene IDs
-system2("zcat",  "Arabidopsis_thaliana.TAIR10.40.gff3.gz | egrep '.+\s.+\sgene\s' | cut -f9 | sed -r 's/ID=gene:([^;]+);.+/\1/g' > Assignments/AthalianaIDs.txt
-")
+system2("zcat",  "Arabidopsis_thaliana.TAIR10.44.gff3.gz | egrep '.+\\s.+\\sgene\\s' | cut -f9 | sed -r 's/ID=gene:([^;]+);.+/\\1/g' > Assignments/AthalianaIDs.txt")
 
 # Now assign each student ID to a gene:
-ids <- read_tsv("enrolments.tsv")
+ids <- read_csv("enrolments.tsv")
 genes <- readLines("Assignments/AthalianaIDs.txt")
 set.seed(7005)
 tibble(ID = paste0("a", ids$Emplid),
